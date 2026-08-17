3f4605b598ab51106f153c751f83d5965b05f0ae
daredakke <Christopher-Cannon@users.noreply.github.com>
2026-08-17 07:06:59 +0100

    Update ResourceLinks.vue

--- src/pages/ResourceLinks.vue
+++ src/pages/ResourceLinks.vue
@@ -1,13 +1,33 @@
 <script setup>
+import ImageGallery from "../components/common/ImageGallery.vue";
+import MainHeading from "../components/common/MainHeading.vue";
+import MinorHeading from "../components/common/MinorHeading.vue";
+import PrevNextNav from "../components/common/PrevNextNav.vue";
+import SideImage from "../components/common/SideImage.vue";
 
+const prevNextNavData = {
+    prevHref: "/glossary",
+    prevLabel: "Glossary",
+    nextHref: "/",
+    nextLabel: "Home",
+};
 </script>
 
 <template>
     <section class="section wrapper">
-        Resource Links
+        <PrevNextNav :data="prevNextNavData" />
+
+        <MainHeading>Resource Links</MainHeading>
+
+        <ul class="list">
+            <li>
+                <a href="#" class="link">Resource Link</a> - A description about
+                the link, such as what information you can find through it.
+            </li>
+        </ul>
+
+        <PrevNextNav :data="prevNextNavData" />
     </section>
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
